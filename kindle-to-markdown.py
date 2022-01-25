b8d0be2f1cbf69847b022e73739a2c121b99e3aa
juicemcpeso <rytmcnally@gmail.com>
2022-01-24 21:02:41 -0800

    Added in functionality to remove duplicates

--- kindle-to-markdown.py
+++ kindle-to-markdown.py
@@ -9,6 +9,7 @@ is_book = False
 is_highlight = False
 is_note = False
 is_bookmark = False
+is_duplicate = False
 
 highlight_text = '- Your Highlight on Location '
 note_text = '- Your Note on Location '
@@ -32,12 +33,17 @@ with open(filename, encoding='utf-8') as file:
         if is_book:
             # Stop logging at the deliminator, reset the book/highlight toggle
             if '==========' in line:
-                list_of_highlights.append(temp_string)
+                if not is_duplicate:
+                    list_of_highlights.append(temp_string)
+
                 temp_string = []
                 
                 is_book = False
                 is_highlight = False
                 is_note = False
+                is_duplicate = False
+
+
 
             # Skip lines with the title in them
             elif desired_title in line:
@@ -54,12 +60,18 @@ with open(filename, encoding='utf-8') as file:
                 location_string = stripped.replace(highlight_text, '')
                 location_int = int(location_string.split(dash_seperator, 1)[0])
                 formatted_header = '## Location ' + location_string
-                    
-                temp_string.append(location_int)
-                # Forces sort to put notes before highlights if same number
-                temp_string.append(1)
-                temp_string.append('\n')
-                temp_string.append(formatted_header.strip())
+
+                if list_of_highlights:
+                    for sublist in list_of_highlights:
+                        if sublist[0] == location_int and sublist[1] == 1:
+                            is_duplicate = True
+
+                if not is_duplicate:
+                    temp_string.append(location_int)
+                    # Forces sort to put notes before highlights if same number
+                    temp_string.append(1)
+                    temp_string.append('\n')
+                    temp_string.append(formatted_header.strip())
 
             # Process note lines
             elif note_text in line:
@@ -67,18 +79,27 @@ with open(filename, encoding='utf-8') as file:
                 stripped = line.split(seperator, 1)[0]
                 update_header = stripped.replace(note_text, '')
                 note_number = int(update_header.strip())
-                
-                temp_string.append(note_number)
-                # Forces sort to put notes before highlights if same number
-                temp_string.append(0)
 
-            # Processing all other lines
+                if list_of_highlights:
+                    for sublist in list_of_highlights:
+                        if sublist[0] == note_number and sublist[1] == 0:
+                            is_duplicate = True
+
+                if not is_duplicate:
+                    temp_string.append(note_number)
+                    # Forces sort to put notes before highlights if same number
+                    temp_string.append(0)
+
+            # Processing text lines
             else:
-                if is_highlight:
-                    added_carrot = ('>' + line.strip() + '\n')
-                    temp_string.append(added_carrot)
-                elif is_note:
-                    temp_string.append(line.strip())
+                if is_duplicate:
+                    pass
+                else:
+                    if is_highlight:
+                        added_carrot = ('>' + line.strip() + '\n')
+                        temp_string.append(added_carrot)
+                    elif is_note:
+                        temp_string.append(line.strip())
 
 sorted_highlights = sorted(list_of_highlights)
 
